48e6dd62c5f9ac0726b3095e5be428e3b00c9083
David N. Robinson <drobinson@raybeam.com>
2014-01-30 10:42:19 -0500

    initialized repo and changes README to markdown

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SocialDice::Application.config.secret_key_base = '23ee574e7348eae4dfab28e60919bd906b2f3d91b19f93f233bd0a299250b5d0e5a65059a41b333d75e68bdcea98966f45c2a089a4b2e52beb8f865850ac2320'
