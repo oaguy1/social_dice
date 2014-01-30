72a4fddba8f7a70e94dd1c1f1882839751a6bdce
David N. Robinson <drobinson@raybeam.com>
2014-01-30 17:08:23 -0500

    Finished adding validations and tests for them. All tests are currently passing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,12 @@
 class User < ActiveRecord::Base
-  validates :email, uniqueness:true
+  before_save { self.email = email.downcase }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name, presence: true
+  validates :email, uniqueness:true, presence: true,
+    format: { with: VALID_EMAIL_REGEX }
+  validates :user_name, uniqueness: true, presence: true
+  validates :password, length: { minimum: 6 }
+
+  has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -16,6 +16,11 @@ describe User do
 
   it { should be_valid }
 
+  describe "when name is not present" do
+    before { @user.name = " " }
+    it { should_not be_valid }
+  end
+
   describe "when email is taken" do
     before do
       user_with_same_email = @user.dup
@@ -23,5 +28,67 @@ describe User do
     end
 
     it { should_not be_valid }
+  end
+
+  describe "when email is blank" do
+    before { @user.email = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when user_name is taken" do
+    before do
+      user_with_same_username = @user.dup
+      user_with_same_username.save
+    end
+
+    it { should_not be_valid }
+  end
+
+  describe "when user_name is blank" do
+    before { @user.user_name = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when format is invalid" do
+    it "should be invalid" do
+      addresses = %w[@bob.com ausers_at_org.org example@. bob@bob. wtf@BOB++.co]
+      addresses.each do |email_address|
+        @user.email = email_address
+        expect(@user).to_not be_valid
+      end
+    end
+  end
+
+  describe "when format is valid" do
+    it "should be valid" do
+      addresses = %w[bob@bob.com library@clarku.edu example@bbc.co.uk]
+      addresses.each do |email_address|
+        @user.email = email_address
+        expect(@user).to be_valid
+      end
+    end
+  end
+
+  describe "when password is not present" do
+    before do
+      @user = User.new(name: "Example User", email: "user@example.com",
+                       user_name: "example123", password: " ",
+                       password_confirmation: " ")
+    end
+    it{ should_not be_valid}
+  end
+
+  describe "when password and confirmation do not match" do
+    before { @user.password_confirmation = "not the password" }
+    it { should_not be_valid}
+  end
+
+  describe "when password is not present" do
+    before do
+      @user = User.new(name: "Example User", email: "user@example.com",
+                       user_name: "example123", password: "123",
+                       password_confirmation: "123")
+    end
+    it{ should_not be_valid}
   end
 end
